3a6931f4091b86e45cd4cf2f3e528e72098c5d9c
Oyewole Moruff <morufoye@gmail.com>
2022-03-22 13:41:36 -0700

    minor change

--- src/main/java/seerBit/backend/assessment/model/Transaction.java
+++ src/main/java/seerBit/backend/assessment/model/Transaction.java
@@ -3,7 +3,6 @@ package seerBit.backend.assessment.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import seerBit.backend.assessment.utility.Utility;
 
 import java.math.BigDecimal;
 import java.sql.SQLOutput;
